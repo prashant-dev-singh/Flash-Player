141afc41676d629767e67788b9772d7207a998d3
prashant-dev-singh <74811257+prashant-dev-singh@users.noreply.github.com>
2021-04-15 01:10:23 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./Components/Home";
-import Wishlist from "./Components/Wishlist";
-import Playlist from "./Components/Playlist";
+import Navlist from "./Components/Navlist";
+
 function App() {
   return (
-    <div className="App">
-      <ul className="navbar">
-        <li>
-          <Link to="/">HOME</Link>
-        </li>
-        <input className="navbar-txtbx"></input>
-
-        <div className="navbar-itm-rgt">
-          <li className="navbar-list">
-            <li>
-              <Link to="/playlist">PLAYLIST</Link>
-            </li>
-          </li>
-          <li>
-            <Link to="/wishlist">WISHLIST</Link>
-          </li>
-        </div>
-      </ul>
-
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/wishlist" element={<Wishlist />}></Route>
-        <Route path="/playlist" element={<Playlist />}></Route>
-      </Routes>
-    </div>
+    <>
+      <Navlist />
+    </>
   );
 }
 
---
+++ src/Components/Navlist.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import Wishlist from "./Wishlist";
+import Playlist from "./Playlist";
+
+export default function Navlist() {
+  return (
+    <>
+      <div className="App">
+        <ul className="navbar">
+          <li>
+            <Link to="/">HOME</Link>
+          </li>
+          <input className="navbar-txtbx"></input>
+
+          <div className="navbar-itm-rgt">
+            <li className="navbar-list">
+              <li>
+                <Link to="/playlist">PLAYLIST</Link>
+              </li>
+            </li>
+            <li>
+              <Link to="/wishlist">WISHLIST</Link>
+            </li>
+          </div>
+        </ul>
+
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/wishlist" element={<Wishlist />}></Route>
+          <Route path="/playlist" element={<Playlist />}></Route>
+        </Routes>
+      </div>
+    </>
+  );
+}

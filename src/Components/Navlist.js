e8ba09c6675e5dcdf3a2fef6b7702af032cb1d49
prashant-dev-singh <74811257+prashant-dev-singh@users.noreply.github.com>
2021-04-17 11:31:00 +0530

    commit

--- src/Components/Navlist.js
+++ src/Components/Navlist.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import Wishlist from "./Wishlist";
 import Playlist from "./Playlist";
